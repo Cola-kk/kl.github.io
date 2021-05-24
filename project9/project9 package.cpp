8598a4e2887bdd2e9f2cde2eb29b2a5bfe2c5369
jinkaili <1071318422@qq.com>
2021-05-24 15:22:47 +0800

    Update project9 package.cpp

--- project9/project9 package.cpp
+++ project9/project9 package.cpp
@@ -1,38 +1,100 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int max(int a, int b)//取最大值函数
+int knapsack_complete(int* W, int* V, int* res, int n, int C)
 {
-    return a > b ? a : b;
+    int value = 0;
+    int** f = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        f[i] = new int[C + 1];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < C + 1; j++)
+            f[i][j] = 0;
+    }
+    for (int y = 1; y < C + 1; y++)
+    {
+        f[0][y] = (y < W[0]) ? 0 : ((y / W[0]) * V[0]);
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int y = 1; y < C + 1; y++)
+        {
+            if (y < W[i])
+            {
+                f[i][y] = f[i - 1][y];
+            }
+            else {
+                f[i][y] = (f[i - 1][y] > (f[i][y - W[i]] + V[i])) ? f[i - 1][y] : (f[i][y - W[i]] + V[i]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < C + 1; j++)
+            cout << f[i][j] << " ";
+        cout << endl;
+    }
+
+    value = f[n - 1][C];
+    int j = n - 1;
+    int y = C;
+    while (j)
+    {
+        if (f[j][y] == (f[j][y - W[j]] + V[j]))
+        {
+            res[j]++;
+            y = y - W[j];
+        }
+        j--;
+    }
+    res[0] = f[0][y] / V[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        delete f[i];
+        f[i] = 0;
+    }
+    delete[] f;
+    f = 0;
+    return value;
 }
-
-struct Thing
+void test1()
 {
-    int w;
-    int v;
-}list[101];
-
-int dp[101][1001];
+    int n, C;
+    while (cin >> n >> C)
+    {
+        int* W = new int[n];
+        int* V = new int[n];
+        int* res = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = 0;
+        }
+        int i = 0, w, v;
+        while (i < n)
+        {
+            cin >> w >> v;
+            W[i] = w;
+            V[i] = v;
+            i++;
+        }
+        int value = knapsack_complete(W, V, res, n, C);
+        cout << value << endl;
+        for (int i = 0; i < n; i++)
+            cout << res[i] << " ";
+        cout << endl;
+        delete W; W = 0;
+        delete V; V = 0;
+        delete res; res = 0;
+    }
+}
 
 int main()
 {
-    int s, n;//背包容量和物品总数
-    cin >> s >> n;
-        for (int i = 1; i <= n; i++)
-        {
-           cin>>list[i].w>>list[i].v;//读入每个物品的体积和价值
-        }
-        for (int i = 0; i <= s; i++) dp[0][i] = 0;//初始化二维数组
-        for (int i = 1; i <= n; i++)//循环每个物品，执行状态转移方程
-        {
-            for (int j = s; j >= list[i].w; j--)
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - list[i].w] + list[i].v);
-            }
-            for (int j = list[i].w - 1; j >= 0; j--)
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
-        cout<<dp[n][s];
+    test1();
     return 0;
 }
